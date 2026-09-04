6573c29b8e096194605876f07a23694f3572fe6d
Doug Rathbone <dougrathbone@gmail.com>
2026-09-04 16:29:54 +1000

    refactor: extract lighting/enable-control discovery publishers into their own mixin
    Keeps haDiscoveryPublishers focused on shared helpers and event-driven
    sensors while lighting and typed-app group discovery live beside security
    and aircon.

--- src/haDiscovery.js
+++ src/haDiscovery.js
@@ -18,10 +18,11 @@ const CONFIG_TOPIC_SUFFIX = `/${HA_DISCOVERY_SUFFIX}`;
 
 /**
  * Methods mixed into HaDiscovery.prototype from haDiscoveryTreeSession.js,
- * haDiscoveryPublishers.js, haDiscoveryPublishersSecurity.js and
- * haDiscoveryPublishersAircon.js at module load (see the Object.assign calls
- * at the bottom of this file). Declared here so calls into the mixin modules
- * type-check; the implementations live in those modules.
+ * haDiscoveryPublishers.js, haDiscoveryPublishersLighting.js,
+ * haDiscoveryPublishersSecurity.js and haDiscoveryPublishersAircon.js at
+ * module load (see the Object.assign calls at the bottom of this file).
+ * Declared here so calls into the mixin modules type-check; the
+ * implementations live in those modules.
  * @typedef {Object} HaDiscoveryMixinMethods
  * @property {(networkId: string|number) => void} queueTreeRequest
  * @property {(networkId: string) => void} _clearTreeState
@@ -836,6 +837,7 @@ class HaDiscovery {
 
 Object.assign(HaDiscovery.prototype, require('./haDiscoveryTreeSession'));
 Object.assign(HaDiscovery.prototype, require('./haDiscoveryPublishers'));
+Object.assign(HaDiscovery.prototype, require('./haDiscoveryPublishersLighting'));
 Object.assign(HaDiscovery.prototype, require('./haDiscoveryPublishersSecurity'));
 Object.assign(HaDiscovery.prototype, require('./haDiscoveryPublishersAircon'));
 module.exports = HaDiscovery;
